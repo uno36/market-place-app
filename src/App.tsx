d46ad9f7f2b30b8268c53df778e7e2cf9c90c4d2
Emmanuel Tofa <emmanuelutofa@gmail.com>
2024-01-20 09:59:09 +0200

    Install bootstrap

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import Header from './components/Header'
 import Carousel from './components/Carousel'
 import Collections from './components/Carousel'
 import Footer from './components/Footer'
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css'
 
 function App() {
